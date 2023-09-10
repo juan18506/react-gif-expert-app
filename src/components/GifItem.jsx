9bf6f4b5c389100e08c6448ee6c17ac999a7461c
juan18506 <juan185067@gmail.com>
2023-09-10 19:09:23 -0300

    Added prop-types to GifItem component

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 export const GifItem = ({ title, url }) => {
   return (
     <li className="card">
@@ -5,4 +7,9 @@ export const GifItem = ({ title, url }) => {
       <p>{ title }</p>
     </li>
   );
+}
+
+GifItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  url  : PropTypes.string.isRequired,
 }
