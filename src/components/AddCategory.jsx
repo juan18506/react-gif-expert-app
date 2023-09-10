c5cd89588ea32e63724872d2bc88f2c6227e1661
juan18506 <juan185067@gmail.com>
2023-09-10 14:22:42 -0300

    Update inputValue state on input change

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -3,11 +3,14 @@ import { useState } from 'react';
 export const AddCategory = () => {
   const [inputValue, setInputValue] = useState('');
 
+  const onInputChange = ({ target }) => setInputValue( target.value );
+
   return (
     <input
       type="text"
       placeholder="Search gifs"
       value={ inputValue }
+      onChange={ onInputChange }
     />
   );
 }
