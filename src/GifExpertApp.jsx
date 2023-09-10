8b895aa58effcbabbce74b49e0000c5f6116ac0b
juan18506 <juan185067@gmail.com>
2023-09-10 13:55:25 -0300

    Render categories array with map method

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,19 @@
+import { useState } from 'react';
+
 export const GifExpertApp = () => {
+  const [categories, setCategories] = useState([ 'Action', 'Drama' ]);
+
   return (
     <>
       <h1>GifExpertApp</h1>
+
+      <ol>
+        { 
+          categories.map( category => (
+            <li key={ category }>{ category }</li>
+          ))
+        }
+      </ol>
     </>
   );
 }
