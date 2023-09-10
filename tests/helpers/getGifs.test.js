4db2522372e8864a26758410f66c1021784c047b
juan18506 <juan185067@gmail.com>
2023-09-10 19:45:52 -0300

    Added getGifs tests

---
+++ tests/helpers/getGifs.test.js
@@ -0,0 +1,16 @@
+import { describe, it, expect } from 'vitest';
+import { getGifs } from '../../src/helpers/getGifs';
+
+describe('Tests in getGifs', () => {
+  const category = 'Fantasy'
+
+  it('should return an array of gifs', async () => {
+    const gifs = await getGifs( category );
+    expect( gifs.length ).toBeGreaterThan( 0 );
+    expect( gifs[0] ).toEqual({
+      id: expect.any( String ),
+      title: expect.any( String ),
+      url: expect.any( String ),
+    });
+  });
+});
