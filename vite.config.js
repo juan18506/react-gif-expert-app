b77aa1429191a56cc56486eef1a92057026468c7
juan18506 <juan185067@gmail.com>
2023-09-11 00:58:42 -0300

    Changed base property to fit gh-pages url

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'react-gif-expert-app',
+  base: '/react-gif-expert-app/',
   plugins: [react()],
   test: {
     globals: true,
