d02d19f9968fd14d78b475b0488a6623114757bc
juan18506 <juan185067@gmail.com>
2023-09-11 00:37:00 -0300

    Added gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'react-gif-expert-app',
   plugins: [react()],
   test: {
     globals: true,
-    environment: "jsdom",
+    environment: 'jsdom',
     setupFiles: ['./vitest.setup.js'],
     clearMocks: true,
   },
